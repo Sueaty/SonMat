c589683e8b2a4e4a4da5a674e7cb690c5b174226
sueaty <suejeong97@naver.com>
2026-03-06 02:55:54 +0900

    Initial Commit

---
+++ SonMatTests/SonMatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SonMatTests.swift
+//  SonMatTests
+//
+//  Created by 조수정 on 3/6/26.
+//
+
+import Testing
+@testable import SonMat
+
+struct SonMatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
